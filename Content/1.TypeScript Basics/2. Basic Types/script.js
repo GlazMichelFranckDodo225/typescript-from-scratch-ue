2c78e979671b9ad9885115dcc438e3797f580c1a
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-03 17:17:45 +0100

    Type Inference

---
+++ Content/1.TypeScript Basics/2. Basic Types/script.js
@@ -0,0 +1,8 @@
+"use strict";
+// Type Inference
+let str = "String";
+let num = 5;
+let array = [];
+let obj = {};
+let toggle = true;
+let anyThing;
---
+++ Content/1.TypeScript Basics/2. Basic Types/script.ts
@@ -0,0 +1,8 @@
+// Type Inference
+let str = "String";
+let num = 5;
+let array = [];
+let obj = {};
+let toggle = true;
+let anyThing;
+
