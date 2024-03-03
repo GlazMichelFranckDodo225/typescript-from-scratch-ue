ec7223483ce02be90d9f38b0b9648e257bcf0fc3
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-03-03 17:26:45 +0100

    Basic Types

--- Content/1.TypeScript Basics/2. Basic Types/script.js
+++ Content/1.TypeScript Basics/2. Basic Types/script.js
@@ -1,8 +1,14 @@
 "use strict";
 // Type Inference
-let str = "String";
-let num = 5;
-let array = [];
-let obj = {};
-let toggle = true;
-let anyThing;
+let str = "String"; // string
+let num = 5; // number
+let array = []; // any[]
+let obj = {}; // {}
+let toggle = true; // boolean
+let anyThing; // any
+// Typing
+let randomNumber;
+const conversion = (celsius) => {
+    return (celsius * (9 / 5)) + 32;
+};
+console.log(conversion(10));
--- Content/1.TypeScript Basics/2. Basic Types/script.ts
+++ Content/1.TypeScript Basics/2. Basic Types/script.ts
@@ -1,8 +1,14 @@
 // Type Inference
-let str = "String";
-let num = 5;
-let array = [];
-let obj = {};
-let toggle = true;
-let anyThing;
+let str = "String"; // string
+let num = 5; // number
+let array = []; // any[]
+let obj = {}; // {}
+let toggle = true; // boolean
+let anyThing; // any
 
+// Typing
+let randomNumber: number;
+const conversion = (celsius: number) => {
+  return (celsius * (9 / 5)) + 32;
+};
+console.log(conversion(10));
